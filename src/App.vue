7e8361eed95d5f1a26b8d030be2dcacff87f433b
C1921 <w915504817@gmail.com>
2025-02-06 18:08:11 +0800

    样式：新增标签页

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,79 @@ import PopulationList from './components/PopulationList.vue'
 <template>
   <div class="min-vh-100 p-4">
     <div class="container-fluid">
-      <div class="row g-4">
-        <div class="col">
+      <ul class="nav nav-tabs mb-4" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button 
+            class="nav-link active" 
+            data-bs-toggle="tab" 
+            data-bs-target="#resources-tab" 
+            type="button" 
+            role="tab" 
+            aria-selected="true"
+          >
+            Resources
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button 
+            class="nav-link" 
+            data-bs-toggle="tab" 
+            data-bs-target="#production-tab" 
+            type="button" 
+            role="tab" 
+            aria-selected="false"
+          >
+            Production
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button 
+            class="nav-link" 
+            data-bs-toggle="tab" 
+            data-bs-target="#mining-tab" 
+            type="button" 
+            role="tab" 
+            aria-selected="false"
+          >
+            Mining
+          </button>
+        </li>
+      </ul>
+
+      <div class="tab-content">
+        <div 
+          class="tab-pane fade show active" 
+          id="resources-tab" 
+          role="tabpanel" 
+          tabindex="0"
+        >
           <div class="d-flex flex-column gap-4">
             <ResourceList />
             <ResourceChart />
             <PopulationList />
             <SpaceshipList />
-            <MiningList />
+          </div>
+        </div>
+
+        <div 
+          class="tab-pane fade" 
+          id="production-tab" 
+          role="tabpanel" 
+          tabindex="0"
+        >
+          <div class="d-flex flex-column gap-4">
             <ProductionList />
+          </div>
+        </div>
+
+        <div 
+          class="tab-pane fade" 
+          id="mining-tab" 
+          role="tabpanel" 
+          tabindex="0"
+        >
+          <div class="d-flex flex-column gap-4">
+            <MiningList />
           </div>
         </div>
       </div>
